8c3175267d68e21cb4b35ca6358785c8d3e6620a
Vineeth1922 <vineethkarri1922@gmail.com>
2025-09-17 18:52:28 +0530

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -9,3 +9,24 @@ sed -i '56d' apache-tomcat-9.0.108/conf/tomcat-users.xml
 sed -i '21d' apache-tomcat-9.0.108/webapps/manager/META-INF/context.xml
 sed -i '22d'  apache-tomcat-9.0.108/webapps/manager/META-INF/context.xml
 sh apache-tomcat-9.0.108/bin/startup.sh
+# Go to /opt (common location for apps)
+cd /opt
+
+# Download latest Tomcat 9 (9.0.109)
+wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.109/bin/apache-tomcat-9.0.109.tar.gz
+
+# Extract it
+tar -xvzf apache-tomcat-9.0.109.tar.gz
+
+# Rename folder for simplicity
+mv apache-tomcat-9.0.109 tomcat9
+
+# Give execute permission to startup/shutdown scripts
+chmod +x tomcat9/bin/*.sh
+
+# Start Tomcat
+./tomcat9/bin/startup.sh
+
+# To stop Tomcat
+# ./tomcat9/bin/shutdown.sh
+
