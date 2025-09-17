6dec542a6fc74da689609acb3c4bbb54f1cafbb2
Vineeth1922 <vineethkarri1922@gmail.com>
2025-09-17 19:34:34 +0530

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -9,11 +9,3 @@ sed -i '56d' apache-tomcat-9.0.108/conf/tomcat-users.xml
 sed -i '21d' apache-tomcat-9.0.108/webapps/manager/META-INF/context.xml
 sed -i '22d'  apache-tomcat-9.0.108/webapps/manager/META-INF/context.xml
 sh apache-tomcat-9.0.108/bin/startup.sh
-cd /opt
-wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.109/bin/apache-tomcat-9.0.109.tar.gz
-tar -xvzf apache-tomcat-9.0.109.tar.gz
-mv apache-tomcat-9.0.109 tomcat9
-chmod +x tomcat9/bin/*.sh
-./tomcat9/bin/startup.sh
-
-
